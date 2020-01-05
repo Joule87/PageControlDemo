7bb527262c77337017a3b3fcf0bf56e5944c1dd5
Julio Collado <jcollado@altimetrik.com>
2020-01-05 16:14:25 -0300

    Initial Commit

---
+++ PageControlDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageControlDemo
+//
+//  Created by Julio Collado on 1/5/20.
+//  Copyright © 2020 Julio Collado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
