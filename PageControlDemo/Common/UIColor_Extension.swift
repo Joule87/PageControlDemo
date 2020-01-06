978f0e0305aa7964e1a4d2b31fb7a5c0fea92f29
Julio Collado <jcollado@altimetrik.com>
2020-01-05 21:01:59 -0300

    PageControlDemo: Dota 2

---
+++ PageControlDemo/Common/UIColor_Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor_Extension.swift
+//  PageControlDemo
+//
+//  Created by Julio Collado on 1/5/20.
+//  Copyright © 2020 Julio Collado. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let blueColor = UIColor(displayP3Red: 0/255, green: 73/255, blue: 244/255, alpha: 1)
+}
